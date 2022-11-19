fac82d1897e25b8ace3ad379c3e847dc7a49c675
rudnitskih <rudnitskih@gmail.com>
2022-11-19 12:35:08 +0200

    Add projects

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+{
   const projects = [
     {
       label: 'litkovska',
@@ -11,16 +11,39 @@ window.addEventListener('DOMContentLoaded', (event) => {
     {
       label: 'sayenko kharenko',
       description: 'visual identity, editorial',
+    },
+    {
+      label: 'gallery',
+      description: 'web',
+    },
+    {
+      label: 'soviet modernism',
+      description: 'editorial',
+    },
+    {
+      label: 'celebrating tigers',
+      description: 'editorial',
+    },
+    {
+      label: 'koolban',
+      description: 'editorial',
     }
   ];
 
-  document.querySelector('.projects').innerHTML = projects.map(({label, description}) => {
-    // language=HTML
-    return `
+  function renderProjects() {
+    document.querySelector('.projects').innerHTML = projects.map(({label, description}) => {
+      // language=HTML
+      return `
       <li class="project">
         <button class="button projectLabel">${label}</button>
         <p class="projectDescription">${description}</p>
       </li>
     `;
-  }).join('');
-});
+    }).join('');
+  }
+
+  window.addEventListener('DOMContentLoaded', (event) => {
+    renderProjects();
+  });
+}
+
