4471bc96f3dc3fea7a53c67105325d235646feec
MakeU2Believe <mykola.a.deineko@gmail.com>
2022-12-05 13:41:26 +0200

    Update projects.ts

--- data/projects.ts
+++ data/projects.ts
@@ -36,7 +36,15 @@ export const projects: Project[] = [
     year: 2018,
     type: 'identity, art direction',
     brief: 'The Litkovska commissioned Hooga for a visual identity update in 2018. The agency’s approach was to anchor the identity around the bespoke tailored wordmark. As part of the team, I was responsible for assets design, including BCs, tags, labels, pe bags, and packaging boxes.',
-    media: [],
+    media: [
+      '1.jpg',
+      '2.jpg',
+      [null, '3_1.jpg'],
+      '4.jpeg',
+      ['5_2.jpg', null],
+      '6.jpg',
+      [null, '7_2.jpg'],
+    ],
   },
   {
     slug: 'sayenko-kharenko',
@@ -45,7 +53,19 @@ export const projects: Project[] = [
     type: 'identity, art direction',
     brief: 'Visual identity facelift for the top-ranked Ukrainian law firm. The project goal was to refine the' +
       ' existing firm’s logo, adjust the color palette, and develop a bold visual approach for how assets across different media are treated. The outcome is an optically balanced icon, refined logotype, and complete package of digital and print templates.',
-    media: [],
+    media: [
+      '1.jpg',
+      '2.jpg',
+      '3.jpg',
+      '4.jpg',
+      '5.jpg',
+      '6.jpg',
+      '7.jpg',
+      '8.jpg',
+      '9.jpg',
+      ['10_2.jpg', null],
+      [null, '11_1.jpg'],
+    ],
   },
   {
     slug: 'celebrating-tigers',
@@ -53,7 +73,7 @@ export const projects: Project[] = [
     year: 2018,
     type: 'editorial, packaging',
     brief: 'Fundraising project to help the “Tiger Canyon” hotel in South Africa. The coffee-table book with wildlife photography by Alex Kirichko. Standard edition of 900 copies, plus 100 premium edition examples packed in a bespoke gift box and came with a unique set of art prints. Designed in collaboration with Dmytro Yarynych. Photography: Alex Kirichko, words: John Varty. Printed by:',
-    media:  [
+    media: [
       '1.jpg',
       '2.jpg',
       '3.jpg',
@@ -80,7 +100,21 @@ export const projects: Project[] = [
     type: 'editorial',
     brief: 'Olexyi Bykov’s photographic homage to works of soviet Ukrainian architects. It displays modernist' +
       ' objects within diverse architectural environment of modern Ukrainian cities. Photogrpahy: Alex Bykov. Words: Ievgeniia Gubkina, Alex Bykov and John Nicholson. Published by: Osnovy. Printed by: DOM. ISBN: 9789665008194',
-    media: [],
+    media: [
+      '1.jpg',
+      '2.jpg',
+      '3.jpg',
+      '4.jpg',
+      '5.jpg',
+      '6.jpg',
+      '7.jpg',
+      '8.jpg',
+      '9.jpg',
+      '10.jpg',
+      '11_1.jpg',
+      '12.jpg',
+      '13.jpg',
+    ],
   },
   {
     slug: 'mriya',
@@ -90,14 +124,21 @@ export const projects: Project[] = [
     brief: 'The largest cargo airplane was destroyed within the first days of russian invasion of Ukraine. It' +
       ' existed in a single example. This personal project is a tribute to such a beautiful and majestic piece of' +
       ' machinery. Printed in an edition of 225 pcs. All of the profit was donated to the volunteers, which help ukrainian armed forces.',
-    media: [],
+    media: [
+      '1.jpg',
+      '2.jpg',
+      '3.jpg',
+      '4.jpg',
+      '5.jpg',
+      [null, '6_1.mp4'],
+    ],
   },
   {
     slug: 'gallery-detailing',
     title: 'gallery detailing',
     year: 2021,
     type: 'web',
-    brief: 'Brand consulting and a web design for the car detailing studio based in Kyiv.',
+    brief: 'Brand consulting and web design for the car detailing studio based in Kyiv.',
     media: [],
   },
   {
@@ -109,7 +150,7 @@ export const projects: Project[] = [
     media: [],
   }
 ].map((project) => {
-  const {slug, media} = project;
+  const { slug, media } = project;
 
   return {
     ...project,
