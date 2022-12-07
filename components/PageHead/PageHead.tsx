ad7680ed12d7b6279aebe9f1e193f85dc307d89b
rudnitskih <rudnitskih@gmail.com>
2022-12-07 11:39:51 +0200

    Add favicon options

--- components/PageHead/PageHead.tsx
+++ components/PageHead/PageHead.tsx
@@ -4,21 +4,22 @@ import Head from 'next/head';
 export interface PageHeadProps {
   title: string;
   description: string;
+  faviconMode?: 'light' | 'dark';
 }
 
 export class PageHead extends React.Component<PageHeadProps> {
   render() {
-    const {title, description} = this.props;
+    const {title, description, faviconMode = 'dark'} = this.props;
+    const faviconSuffix = faviconMode === 'light' ? '-light' : '';
+
 
     return (
       <Head>
         <title>{title}</title>
         <meta name="description" content={description}/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="icon" href={`/favicon${faviconSuffix}.ico`} sizes="any" />
+        <link rel="icon" href={`/favicon${faviconSuffix}.svg`} type="image/svg+xml" />
       </Head>
     );
   }
