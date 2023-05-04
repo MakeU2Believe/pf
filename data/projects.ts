1d652178455072584cf925cb99b75cf2686175ab
MakeU2Believe <117759424+MakeU2Believe@users.noreply.github.com>
2023-05-04 21:17:36 +0300

    S&K content updt

--- data/projects.ts
+++ data/projects.ts
@@ -56,7 +56,7 @@ const rawProjects: RawProject[] = [
     title: 'Sayenko Kharenko',
     year: 2021,
     type: 'visual identity',
-    brief: 'Visual identity facelift for the top-ranked ukrainian law firm. The project goal was to refine the' +
+    brief: 'Visual identity facelift for the top-ranked Ukrainian law firm. The project goal was to refine the' +
       ' existing firm’s logo, adjust the color palette, and develop a bold visual approach for how assets across different media are treated. The outcome is an optically balanced icon, refined logotype, and complete package of digital and print templates. Done with LY studio.',
     media: [
       '1.webp',
